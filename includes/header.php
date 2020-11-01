17082a804d223900d887ae97acd3598c2bbb39d3
jbbreil <65733113+jbbreil@users.noreply.github.com>
2020-11-01 22:40:14 +0100

    Add files via upload

---
+++ includes/header.php
@@ -0,0 +1,18 @@
+<?php include("config.php"); ?>
+<!DOCTYPE html>
+<html lang="sv">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="description" content="CV/portfolio">
+    <meta name="keywords" content="HTML,SASS,PHP,JavaScript">
+    <meta name="author" content="Gianluca Incandela">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <!-- Fetch calls JS -->
+    <script defer src="js/fetchAllin.js"></script>
+    <!-- CSS -->
+     <link rel="stylesheet" type="text/css" href="css/sass/main.css">
+    <!-- bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"/>
+    <!-- jQuery -->
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
